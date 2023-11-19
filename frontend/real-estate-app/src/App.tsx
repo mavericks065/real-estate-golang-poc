5ff8c013af9a6f0069abce581b9765950a8bdbe0
Nicolas Guignard <nicolas065@gmail.com>
2023-11-19 18:47:54 +0100

    feat: remove create react

--- frontend/real-estate-app/src/App.tsx
+++ frontend/real-estate-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -14,7 +13,7 @@ function App() {
   const [data, setData] = useState<Ads | undefined>();
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/ads").then(async response => {
+    fetch("http://localhost:9000/api/ads").then(async response => {
       const results = await response.json();
       setData(results);
     })
@@ -23,20 +22,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       {data !== undefined ?
           <ul>
             {data.ads.map(x => {
