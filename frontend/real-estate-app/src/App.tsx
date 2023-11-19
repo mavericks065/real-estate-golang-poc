9b5823f988cf2fed299fa1147cc62f8c19859e71
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2023-11-19 18:53:47 +0100

    Add oidc-client-ts

--- frontend/real-estate-app/src/App.tsx
+++ frontend/real-estate-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import { UserManager } from 'oidc-client-ts';
 
 
 interface Ads {
@@ -10,6 +11,19 @@ interface Ads {
 }
 
 function App() {
+  const userManager = new UserManager({
+    authority: "http://localhost:8080",
+    client_id: "",
+    client_secret: "",
+    redirect_uri: "/callback",
+  });
+
+  useEffect(() => {
+    userManager.getUser().then(user => {
+      console.log(user);
+    })
+  }, [])
+
   const [data, setData] = useState<Ads | undefined>();
 
   useEffect(() => {
@@ -18,7 +32,6 @@ function App() {
       setData(results);
     })
   }, []);
-
 
   return (
     <div className="App">
